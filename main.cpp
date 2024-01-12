12e1397a15eeac222df3a6e493ce6197659a1504
sp1rale <dimayaremchyk24@gmail.com>
2024-01-12 21:08:50 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+class Pet {
+public:
+    Pet(const std::string& name) : name(name) {}
+    virtual void makeSound() const = 0;
+    const std::string& getName() const {
+        return name;
+    }
+
+private:
+    std::string name;
+};
+
+class Dog : public Pet {
+public:
+    Dog(const std::string& name) : Pet(name) {}
+    void makeSound() const override {
+        std::cout << getName() << " voice-Woof!!!\n";
+    }
+};
+
+class Cat : public Pet {
+public:
+    Cat(const std::string& name) : Pet(name) {}
+
+    void makeSound() const override {
+        std::cout << getName() << " says Meow...murrr!!!\n";
+    }
+};
+
+class Parrot : public Pet {
+public:
+    Parrot(const std::string& name) : Pet(name) {}
+
+    void makeSound() const override {
+        std::cout << getName() << " says Hail!!!\n";
+    }
+};
+
+int main() {
+    Dog myDog("Blondi");
+    Cat myCat("Simba");
+    Parrot myParrot("Jasper");
+
+    myDog.makeSound();
+    myCat.makeSound();
+    myParrot.makeSound();
+
+    return 0;
 }
