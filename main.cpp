5ac076cd077b338276f7aac8ad21e08744e62012
sp1rale <dimayaremchyk24@gmail.com>
2024-01-12 20:21:13 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+class Student {
+protected:
+    std::string name;
+    int age;
+    double averageGrade;
+
+public:
+    Student(const std::string& n, int a, double avg) : name(n), age(a), averageGrade(avg) {}
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << "\nAge: " << age << "\nAverage Grade: " << averageGrade << std::endl;
+    }
+};
+
+class Aspirant : public Student {
+private:
+    std::string thesisTopic;
+
+public:
+    Aspirant(const std::string& n, int a, double avg, const std::string& topic)
+        : Student(n, a, avg), thesisTopic(topic) {}
+
+    void displayInfo() const {
+        Student::displayInfo();
+        std::cout << "Thesis Topic: " << thesisTopic << std::endl;
+    }
+};
+
+int main() {
+    Student student("Bill Gates", 27,90.0);
+    std::cout << "Student Information:\n";
+    student.displayInfo();
+    std::cout << "\n";
+
+    Aspirant aspirant("Melinda Gates)", 35,88.5, "Research on Artificial Intelligence");
+    std::cout << "Aspirant Information:\n";
+    aspirant.displayInfo();
+
+    return 0;
 }
